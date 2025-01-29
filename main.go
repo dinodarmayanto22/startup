9df425b36e09ff4e3312fc3841997c59f90f251b
dinodarmayanto22 <masbagus1581@gmail.com>
2025-01-30 04:32:42 +0700

    update code

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/startup/user"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// 	dsn := "host=127.0.0.1 user=dino password=dino1234 dbname=startup_crowdfunding port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+	// 	fmt.Println("Connection to database is good")
+
+	// 	var users []user.User
+	// 	length := len(users)
+
+	// 	fmt.Println(length)
+
+	// 	db.Find(&users)
+
+	// 	length = len(users)
+	// 	fmt.Println(length)
+
+	// 	for _, user := range users {
+	// 		fmt.Println(user.Name)
+	// 		fmt.Println(user.Email)
+	// 		fmt.Println("=================")
+
+	// 	}
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "host=127.0.0.1 user=dino password=dino1234 dbname=startup_crowdfunding port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("Connection to database is good")
+
+	var users []user.User
+	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
 }
