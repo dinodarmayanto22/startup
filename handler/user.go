fd41c511dd483de84598f57ad4382511c8ae5b76
dinodarmayanto22 <masbagus1581@gmail.com>
2025-02-01 06:50:01 +0700

    update code helper

--- handler/user.go
+++ handler/user.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/startup/helper"
 	"github.com/startup/user"
 )
 
@@ -16,21 +17,25 @@ func NewUserHandler(userService user.Service) *userHandler {
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
-
 	var input user.RegisterUserInput
 
+	// Bind JSON input
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
-
+		response := helper.APIResponse("Invalid request payload", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return // Pastikan untuk mengembalikan setelah mengirim respons
 	}
 
+	// Register user
 	user, err := h.userService.RegisterUser(input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
-
+		response := helper.APIResponse("Failed to register user", http.StatusInternalServerError, "error", nil)
+		c.JSON(http.StatusInternalServerError, response)
+		return // Pastikan untuk mengembalikan setelah mengirim respons
 	}
 
-	c.JSON(http.StatusOK, user)
-
+	// Jika berhasil, kirim respons sukses
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+	c.JSON(http.StatusOK, response)
 }
